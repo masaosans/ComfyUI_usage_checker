e68cd627d00082f61152c42256876b75d9d9fbdc
masaosans <ms1222ms@gmail.com>
2026-02-26 17:10:49 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .usage_checker import ComfyUIUsageChecker
+
+NODE_CLASS_MAPPINGS = {
+    "ComfyUIUsageChecker": ComfyUIUsageChecker
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "ComfyUIUsageChecker": "ComfyUI Global Usage Checker"
+}
